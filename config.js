4e038a6f712178b3775b63a659f45e2ff9802b56
Austin Li <liaustin926@gmail.com>
2021-01-21 18:30:57 -0500

    crudapp

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://austinli246:austin63026302@cluster0.6vxr6.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
+
+//XQ1dcGLItWFFjZLd
+//mongodb+srv://austinli246:austin63026302@cluster0.6vxr6.mongodb.net/merng?retryWrites=true&w=majority
